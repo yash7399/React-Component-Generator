5b674f9c8fa56ef1197e900e52cc4d4d70312e0d
Yash Shukla <119918899+yash7399@users.noreply.github.com>
2025-07-29 10:53:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,9 +14,7 @@ const PORT=process.env.PORT || 5000;
 
 
 app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:5173"
-}))
+app.use(cors())
 
 app.use("/api/auth",authRouter)
 app.use('/api/generate', generatorRoutes);
